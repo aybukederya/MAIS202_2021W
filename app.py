779c34c43eba1485396043758003584230e92c5d
Aybuke Derya Ekiz <68646100+aybukederya@users.noreply.github.com>
2021-07-10 20:42:04 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template, url_for, request, redirect, jsonify, flash
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+#from flask import Flask, render_template, request, session, Session
+from flask import *
 from joblib import load
 import random
-from string import Template
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+
+
+#not needed imports
+#from flask import url_for, redirect, jsonify, flash
+#from datetime import datetime
+#from string import Template
+#from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
 
 
 # Instantiate your models
 
-clf = load("classifier.joblib")
+clf = load("classifier_new.joblib")
 
 
 # load numpy array from csv file
@@ -20,9 +24,11 @@ import pandas as pd
 # load array
 X_test = loadtxt('X_test.csv', delimiter=',')
 
+
 df = pd.read_csv('y_test.csv',sep='\t', header=None)
 arr = np.array(df)
 y_test = arr.flatten()
+
 
 
 df = pd.read_csv('titles.csv',sep='\t', header=None)
@@ -36,73 +42,105 @@ title_list = arr.flatten()
 
 
 
+
 app = Flask(__name__)
-
-global round_num 
-global your_score 
-global computer_score 
-global five_random_piece_index
-global i
+#SESSION_TYPE = 'filesystem'
+#app.config.from_object(__name__)
+app.secret_key = "abc"  
 
 
-round_num = 0
-your_score = 0
-computer_score = 0
 
-five_random_piece_index = []
-y_test_length = len(y_test)
 
-i = 0
-while i!=5:
-    n = random.randint(0,y_test_length-1)
-    if n not in five_random_piece_index:
-        five_random_piece_index.append(n)
-        i = i+1
+
+
 
 
 # Base endpoint to perform prediction.
 @app.route('/', methods=['POST', 'GET'])
-def play():
-    global your_score
-    global computer_score
-    global round_num
 
-    if round_num == 5:
-        if your_score > computer_score:
+
+def setup():
+    y_test_length = len(y_test)
+    if request.method == 'POST':
+        if request.form['challenge'] == 'YES!':
+            session["round_num"] = 0
+            session["your_score"] = 0 
+            session["computer_score"] = 0
+            session["five_random_piece_index"] = []
+            session["i"] = 0
+            print("initialized")
+
+        while session["i"]!=5:
+            n = random.randint(0,y_test_length-1)
+            if n not in session["five_random_piece_index"]:
+                session["five_random_piece_index"].append(n)
+                session["i"] += 1
+    
+    if request.method == 'POST':
+        if (request.form['challenge'] == 'YES!' or request.form['challenge'] == 'Ready!') and session["round_num"] != 5:
+            piece = "music/" + str(session["five_random_piece_index"][session["round_num"]]) + ".m4a"
+            return render_template('play.html', your_score =  session["your_score"], computer_score = session["computer_score"], round_num = session["round_num"], piece = piece)
+        elif session["round_num"] == 5:
+            if session["your_score"] > session["computer_score"]:
+                winner = "You won!"
+            elif session["your_score"] < session["computer_score"]:
+                winner = "Computer won!"
+            else:
+                winner = "It is a tie!"
+
+            your_final_score = session["your_score"]
+            computer_final_score = session["computer_score"]
+            return render_template("end.html", winner = winner, your_final_score = your_final_score, computer_final_score = computer_final_score)
+    else:
+        return render_template("index.html")
+
+    
+    
+
+
+def play():
+
+    if session["round_num"] == 5:
+        if session["your_score"] > session["computer_score"]:
             winner = "You won!"
-        elif your_score < computer_score:
+        elif session["your_score"] < session["computer_score"]:
             winner = "Computer won!"
         else:
             winner = "It is a tie!"
 
-        your_final_score = your_score
-        computer_final_score = computer_score
+        your_final_score = session["your_score"]
+        computer_final_score = session["computer_score"]
         return render_template("end.html", winner = winner, your_final_score = your_final_score, computer_final_score = computer_final_score)
 
     if request.method == 'POST':
         if request.form['challenge'] == 'YES!' or request.form['challenge'] == 'Ready!':
-            piece = "music/" + str(five_random_piece_index[round_num]) + ".m4a"
-            return render_template('play.html', your_score =  your_score, computer_score = computer_score, round_num = round_num, piece = piece)
+            piece = "music/" + str(session["five_random_piece_index"][session["round_num"]]) + ".m4a"
+            return render_template('play.html', your_score =  session["your_score"], computer_score = session["computer_score"], round_num = session["round_num"], piece = piece)
+    else:
+        return render_template("index.html")
+
+    '''
+    if request.method == 'POST':
+        if request.form['challenge'] == 'YES!' or request.form['challenge'] == 'Ready!':
+            piece = "music/" + str(session["five_random_piece_index"][session["round_num"]]) + ".m4a"
+            return render_template('play.html', your_score =  session["your_score"], computer_score = session["computer_score"], round_num = session["round_num"], piece = piece)
         else:
             return "there was a problem :("
-
     else:
         return render_template('index.html')
+    '''
 
 @app.route('/play', methods=['POST', 'GET'])
 def user_guess():
-    global your_score
-    global computer_score
-    global round_num
 
 
     if request.method == 'POST':
         user_prediction = request.form['user_guess']
-        computer_prediction = str(clf.predict(X_test[five_random_piece_index[round_num]].reshape(1,-1))[0])
-        correct_composer = y_test[five_random_piece_index[round_num]]
-        canonical_title = title_list[five_random_piece_index[round_num]]
+        computer_prediction = str(clf.predict(X_test[session["five_random_piece_index"][session["round_num"]]].reshape(1,-1))[0])
+        correct_composer = y_test[session["five_random_piece_index"][session["round_num"]]]
+        canonical_title = title_list[session["five_random_piece_index"][session["round_num"]]]
 
-        round_num = round_num + 1
+        session["round_num"] = session.get("round_num") + 1
 
         user_prediction_image = user_prediction.split()[-1].lower()+".jpg"
         computer_prediction_image = computer_prediction.split()[-1].lower()+".jpg"
@@ -115,16 +153,16 @@ def user_guess():
 
         if user_prediction == correct_composer and computer_prediction != correct_composer:
             only_user_correct = True
-            your_score = your_score + 1
+            session["your_score"] = session["your_score"] + 1
         
         if computer_prediction == correct_composer and user_prediction != correct_composer:
             only_computer_correct = True
-            computer_score = computer_score +1 
+            session["computer_score"] = session["computer_score"] +1 
         
         if user_prediction == correct_composer and computer_prediction == correct_composer:
             both_correct = True
-            your_score = your_score + 1
-            computer_score = computer_score + 1
+            session["your_score"] = session["your_score"] + 1
+            session["computer_score"] = session["computer_score"] + 1
         
         if user_prediction != correct_composer and computer_prediction != correct_composer:
             both_false = True
@@ -132,39 +170,31 @@ def user_guess():
         return render_template('results.html', user_prediction = user_prediction, computer_prediction = computer_prediction, correct_composer = correct_composer, user_prediction_image = user_prediction_image, computer_prediction_image = computer_prediction_image, correct_composer_image = correct_composer_image, only_user_correct = only_user_correct, only_computer_correct = only_computer_correct, both_correct = both_correct, both_false = both_false, canonical_title = canonical_title)
     
 
-    else:
-        piece = "music/" + str(five_random_piece_index[round_num]) + ".m4a"
-        return render_template('play.html', your_score = your_score, computer_score = computer_score, round_num = round_num, piece = piece)
+    #else:
+        #piece = "music/" + str(five_random_piece_index[round_num]) + ".m4a"
+        #return render_template('play.html', your_score = your_score, computer_score = computer_score, round_num = round_num, piece = piece)
 
 @app.route('/end', methods=['POST', 'GET'])
 def play_again():
-    global your_score
-    global computer_score
-    global round_num
-    global five_random_piece_index
-    global i
-
+    y_test_length = len(y_test)
     if request.method == 'POST' and request.form['play_again'] == "YES!":
-        your_score = 0
-        computer_score = 0
-        round_num = 0
-        five_random_piece_index = []
-        i = 0
-        while i!=5:
+        session["round_num"]=0
+        session["your_score"]=0 
+        session["computer_score"]=0 
+        session["five_random_piece_index"]=[]
+        session["i"]=0
+        while session["i"]!=5:
             n = random.randint(0,y_test_length-1)
-            if n not in five_random_piece_index:
-                five_random_piece_index.append(n)
-                i = i+1
+            if n not in session["five_random_piece_index"]:
+                session["five_random_piece_index"].append(n)
+                session["i"] = session["i"]+1
 
         return render_template("index.html")
 
-    else:
-        return "Bye!"
+    #else:
+        #return "Bye!"
     
 
-
-
-    
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
